c7e732a943fb4eb270372be5a095d6044d94d3ae
HangZhouDesktop <zdh6zox@163.com>
2024-12-30 01:46:37 +0800

    Use New ImGui plugin which support Imgui_Docking

--- PluginsProject/Source/PluginsProject/PluginsProject.Build.cs
+++ PluginsProject/Source/PluginsProject/PluginsProject.Build.cs
@@ -8,7 +8,7 @@ public class PluginsProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ImGui" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
---
+++ PluginsProject/Source/PluginsProject/TestActor.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestActor.h"
+#include <imgui.h>
+
+// Sets default values
+ATestActor::ATestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	const ImGui::FScopedContext ScopedContext;
+	if (ScopedContext)
+	{
+		// Your ImGui code goes here!
+		//ImGui::ShowDemoWindow();
+		ImGui::Begin(TCHAR_TO_UTF8(*GetActorLabel()));
+		ImGui::Button("TestButton");
+		ImGui::End();
+	}
+}
+
